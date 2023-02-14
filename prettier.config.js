6e57e9ad8f135431a5f64ab9ac44901cf7d27a62
Frad LEE <fradser@gmail.com>
2023-02-14 19:29:06 +0800

    build(yarn): add prettier and realease-it

---
+++ prettier.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  parser: 'typescript',
+  arrowParens: 'always',
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 80,
+  semi: false,
+  overrides: [
+    {
+      files: ['*.json'],
+      options: {
+        parser: 'json',
+      },
+    },
+    {
+      files: ['*.md'],
+      options: {
+        parser: 'markdown',
+      },
+    },
+  ],
+}
